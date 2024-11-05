5491c15e06da733451634c141d1fac6dd10c651b
Karolina Nowicka <nowicka.karolina@icloud.com>
2024-11-05 21:56:52 +0100

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,12 +1,17 @@
-def seq_sort(sequence: list[int]) -> list[int]:
+def seq_sort(sequence: list[int], desc: bool=False) -> list[int]:
     size = len(sequence)
 
     for i in range(size):
         for j in range(0, size-i-1):
-            if sequence[j] > sequence[j+1]:
-                sequence[j], sequence[j+1] = sequence[j+1], sequence[j]
+            if desc:
+                if sequence[j] < sequence[j+1]:
+                    sequence[j], sequence[j+1] = sequence[j+1], sequence[j]
+            else:
+                if sequence[j] > sequence[j+1]:
+                    sequence[j], sequence[j+1] = sequence[j+1], sequence[j]
 
     return sequence
 
 if __name__ == "__main__":
     print(seq_sort([6,5,4,99,3,2,1]))
+    print(seq_sort([6,5,4,99,3,2,1], True))
